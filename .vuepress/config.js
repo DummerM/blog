30db4c194e49abbdf748ba8f6843c86f16761a12
xinfangfang <550212808@qq.com>
2022-06-21 23:10:42 +0800

    created!

---
+++ .vuepress/config.js
@@ -0,0 +1,90 @@
+module.exports = {
+  "title": "心方方",
+  "description": "心方方的秘密花园",
+  "dest": "public",
+  plugins: [
+    [
+      "vuepress-plugin-live2d",
+      {
+        "modelName": "",
+        "mobileShow": false,
+        position: "left"
+      }
+    ]
+  ],
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/avatar.png"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "首页",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "时间线",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      // {
+      //   "text": "文档",
+      //   "icon": "reco-message",
+      //   "items": [
+      //     {
+      //       "text": "vuepress-reco",
+      //       "link": "/docs/theme-reco/"
+      //     }
+      //   ]
+      // },
+      {
+        "text": "联系方式",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitHub",
+            "link": "https://github.com/DummerM/DummerM.github.io",
+            "icon": "reco-github"
+          }
+        ]
+      }
+    ],
+    subSidebar: 'auto',
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "类别"
+      },
+      "tag": {
+        "location": 3,
+        "text": "标签"
+      }
+    },
+    "logo": "/avatar.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "lastUpdated": "Last Updated",
+    "author": "xingfangfang",
+    "authorAvatar": "/avatar.png",
+    "record": "xxxx",
+    "startYear": "2022"
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+}
